85647a59d7f3c17ac2cabfe3639f879f190e7fae
Maria <lrabin@gmail.com>
2019-04-26 23:44:38 -0400

    Final Updates

--- game.js
+++ game.js
@@ -1,45 +1,93 @@
 //Variables to use
+
 var APIKey = "IXHrCW7HbVSkilRoks118mHFZJi08MKa";
 var limit = 10;
-var keyword = $("#typeBaby").val().trim();
 
+//initial array of disney giphys
+var topics = ["Elsa", "Rapunzel", "Belle", "Cinderella", "Ariel", "Jasmine", "Anna", "Ursula", "Tigger", "Moana", "Megara", "Simba"];
 
+var index = 0; // for empty search result 
+
+var move = {}; // for still & moving 
+var addPair = function (myKey, myValue) {
+    move[myKey] = myValue;
+};
+var giveValue = function (myKey) {
+    return move[myKey];
+};
+
+//when the images are clicked
 $('body').on('click', 'img', function (e) {
     var imgObj = e.currentTarget;
-    imgObj.setAttribute('src', imgObj.getAttribute('animateurl'));
+    const imageid = imgObj.getAttribute('id');
+    var moveValue = giveValue(imageid);
+    if (moveValue === undefined || moveValue === null) {
+        addPair(imageid, 0); //default which is still
+    }
+    moveValue = giveValue(imageid);
+
+    //still to move
+    if (moveValue === 0) {
+        imgObj.setAttribute('src', imgObj.getAttribute('animateUrl'));
+        addPair(imageid, 1);
+
+        //move to still
+    } else if (moveValue === 1) {
+        imgObj.setAttribute('src', imgObj.getAttribute('stillUrl'));
+        addPair(imageid, 0);
+    }
 });
 
-$("#look").click(function (e) {
-    e.preventDefault();
-    var name = $("#typeBaby").val().trim();
-    $("#list").append(`<button class="baby-btn" style="margin-right:10px; margin-top:10px;" >${name}</button>`);
-    $("#typeBaby").val("");
 
-    //use our AJAX Call
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=" + APIKey + "&limit=" + limit;
-    $.ajax(
-        {
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            var data = response.data;
-            $(data).each(function (i, value) {
-                var imageId = value.id,
-                    imageUrlStill = value.images.original_still.url,
-                    imageUrlAnim = value.images.original.url,
-                    rating = value.rating;
-                $("#graphics").append(`${rating}<img animateUrl=${imageUrlAnim} src=${imageUrlStill} height="100" width="100">`);
+//when the submit button is clicked
+function displayDisney(searchedQuery) {
+    index = 0;
+    $("#graphics-view").empty();
 
-            });
+    if (searchedQuery === undefined || searchedQuery === '' || searchedQuery === null) {
+        searchedQuery = $("#giphy-input").val();
+    }
 
+    if (searchedQuery === '') {
+        return;
+    }
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchedQuery + "&api_key=" + APIKey + "&limit=" + limit;
+
+
+    if (!(topics.map(function (item) { return item.toLowerCase() }).indexOf(searchedQuery.toLowerCase()) != -1)) {
+        topics.push(searchedQuery);
+        const buttonSearched = "<button style='margin-top:10px;' onClick=displayDisney('" + searchedQuery + "') type='button' class='btn btn-primary'>" + searchedQuery + "</button>";
+        $("#buttons-view").append(buttonSearched).append('&nbsp;');
+    }
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        response.data.forEach(element => {
+            index++;
+            const gif_image_object = element.images;
+            const gif_image_still_src = gif_image_object.original_still.url;
+            const gif_image_anim_src = gif_image_object.original.url;
+            const rating = element.rating;
+            const imgid = element.id;
+            $("#graphics-view").append(`${rating} ➡️  <img id=${imgid} animateUrl=${gif_image_anim_src} stillUrl=${gif_image_still_src} src=${gif_image_still_src} height='100' width='100' />`);
         });
+        if (index === 0) { //if nothing was returned
+            const buttonSearched = "<button onClick=displayDisney('" + searchedQuery + "') type='button' class='btn btn-secondary'>No Result Found For: " + searchedQuery + " </button>";
+            $("#graphics-view").append(buttonSearched).append('&nbsp;');
+        }
+    });
+    return false; //Using forms submit always return false so that the page would not refresh on Submit event
+};
 
 
-
+//when page is loaded shows the initial array displays
+$(document).ready(function () {
+    for (var i = 0; i < topics.length; i++) {
+        const btnName = topics[i];
+        const buttonSearched = "<button style='margin-top:10px;' onClick=displayDisney('" + btnName + "'); type='button' class='btn btn-primary'>" + btnName + "</button>";
+        $("#buttons-view").append(buttonSearched).append('&nbsp;');
+    }
 });
-
-
-
-
-
